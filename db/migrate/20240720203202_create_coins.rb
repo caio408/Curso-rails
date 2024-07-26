bdde2d82a810ad27ef4a9f57a219f73301926574
caio408 <caiogomes408@gmail.com>
2024-07-26 00:38:35 +0000

    fixing

--- db/migrate/20240720203202_create_coins.rb
+++ db/migrate/20240720203202_create_coins.rb
@@ -1,9 +1,9 @@
 class CreateCoins < ActiveRecord::Migration[5.2]
   def change
     create_table :coins do |t|
-      t.strinh :description
+      t.string :description
       t.string :acronym
-      t.string :url-image
+      t.string :url_image
 
       t.timestamps
     end
